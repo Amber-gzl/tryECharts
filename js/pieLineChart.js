4018297ba23596a047b763a523f7b52399a0bfac
gaozheling <Ambergaozl@163.com>
2020-08-17 16:38:08 +0800

    改造官网例子成设计图样

--- js/pieLineChart.js
+++ js/pieLineChart.js
@@ -13,17 +13,28 @@ setTimeout(function () {
         },
         dataset: {
             source: [
-                ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
-                ['Matcha Latte', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7],
-                ['Milk Tea', 86.5, 92.1, 85.7, 83.1, 73.4, 55.1],
-                ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5],
-                ['Walnut Brownie', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1]
+                ['product', '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+                ['A部门', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7,41.1, 30.4, 65.1, 53.3, 83.8, 98.7],
+                ['B部门', 86.5, 41.1, 30.4, 65.1, 53.3, 83.8, 98.7,92.1, 85.7, 83.1, 73.4, 55.1],
+                ['C部门', 24.1, 67.2, 41.1, 30.4, 65.1, 53.3, 83.8, 98.7,79.5, 86.4, 65.2, 82.5],
+                ['D部门', 55.2, 67.1, 69.2, 41.1, 30.4, 65.1, 53.3, 83.8, 98.7,72.4, 53.9, 39.1],
+                ['E部门', 86.5, 92.1, 85.7, 83.1, 41.1, 30.4, 65.1, 53.3, 83.8, 98.7,73.4, 55.1],
+                ['F部门', 24.1, 67.2, 41.1, 30.4, 65.1, 53.3, 83.8, 98.7,79.5, 86.4, 65.2, 82.5],
+                ['其他部门', 55.2, 67.1, 69.2, 72.4, 53.9, 41.1, 30.4, 65.1, 53.3, 83.8, 98.7,39.1]
             ]
         },
         xAxis: {type: 'category'},
-        yAxis: {gridIndex: 0},
+        yAxis: [
+            {
+                type: 'value',
+                name: ' 请求数（次）'
+            }
+        ],
         grid: {top: '55%'},
         series: [
+            {type: 'line', smooth: true, seriesLayoutBy: 'row'},
+            {type: 'line', smooth: true, seriesLayoutBy: 'row'},
+            {type: 'line', smooth: true, seriesLayoutBy: 'row'},
             {type: 'line', smooth: true, seriesLayoutBy: 'row'},
             {type: 'line', smooth: true, seriesLayoutBy: 'row'},
             {type: 'line', smooth: true, seriesLayoutBy: 'row'},
@@ -34,12 +45,12 @@ setTimeout(function () {
                 radius: '30%',
                 center: ['50%', '25%'],
                 label: {
-                    formatter: '{b}: {@2012} ({d}%)'
+                    formatter: '{b}: {@十二月} ({d}%)'
                 },
                 encode: {
                     itemName: 'product',
-                    value: '2012',
-                    tooltip: '2012'
+                    value: '十二月',
+                    tooltip: '十二月'
                 }
             }
         ]
